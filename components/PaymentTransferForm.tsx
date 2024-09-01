38fa6aded7b391c3f2b519a96105145c5cee1839
Vaibhav Dipesh Parikh <parikhvaibhav19@gmail.com>
2024-09-01 14:11:09 -0400

    transaction page working and multiple bank accounts

--- components/PaymentTransferForm.tsx
+++ components/PaymentTransferForm.tsx
@@ -29,7 +29,7 @@ import { BankDropdown } from "./BankDropdown";
 const formSchema = z.object({
   email: z.string().email("Invalid email address"),
   name: z.string().min(4, "Transfer note is too short"),
-  amount: z.string().min(4, "Amount is too short"),
+  amount: z.string().min(2, "Amount is too short"),
   senderBank: z.string().min(4, "Please select a valid bank account"),
   sharableId: z.string().min(8, "Please select a valid sharable Id"),
 });
